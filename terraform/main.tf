22076ab1fadb2c9f39aa87a03507c0034b325bda
Rupinder SIngh <enggrupinder@gmail.com>
2020-11-16 01:18:35 +0530

    added backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,13 @@ provider "aws" {
   # profile                 = "development"
 }
 
+terraform {
+      backend "artifactory" {
+        username = "admin"
+        # never actually put passwords in code, but that's beyond our scope today
+        password = "Rupi@123"
+        url      = "http://localhost:8082/artifactory"
+        repo     = "terrform-test"
+        subpath  = "terraform-state"
+      }
+    }
